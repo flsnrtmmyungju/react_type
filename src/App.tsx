cf2f56036a0a093925758387458ef3cb6987e288
flsnrtmmyungju <mjmj01617@naver.com>
2023-07-17 17:05:44 +0900

    ~8.8 드래그해서 useMotionValue useTransform 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -29,31 +19,40 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  hover: { rotateZ: 90 },
-  click: { borderRadius: "100px" },
-};
-
 function App() {
-  //레퍼런스를 잡아와서
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  //                        value,    범위 ,    범위마다 갖고싶은 아웃풋
+  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0]);
+  useEffect(() => {
+    scale.on("change", () => console.log(scale.get()));
+  }, [x]);
+
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          //드래그놓으면 원래위치로 가게
-          dragSnapToOrigin
-          //탄성 0~1사이값만 마우스를 정확히따라오지 않고 당기는힘작용 1로하면 없어짐
-          dragElastic={0.5}
-          //드래그 한계를 biggerBoxRef까지로 잡음
-          dragConstraints={biggerBoxRef}
-          variants={boxVariants}
-          whileHover="hover"
-          whileTap="click"
-        />
-      </BiggerBox>
+      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
 export default App;
+
+//^ 기본적인 useMotionValue사용
+// function App() {
+//   reactjs state가 아니라 react rendering cycle 발동 되지않음.변화가감지되지않아서 콘솔도안찍힘
+//   const x = useMotionValue(0);
+//   useEffect써야 변화감지가능
+//   useEffect(() => {x.on("change", () => console.log(x.get()));}, [x]);
+
+//   return (
+//     <Wrapper>
+//        메뉴얼로 x값변경하는 방법
+//        <button onClick={() => x.set(200)}>cliclk me!</button>
+//       <Box
+//         useMotionValue랑 값 연결하기위해
+//         style={{ x }}
+//         drag="x"
+//         드래그놓으면 원래위치로 가게
+//         dragSnapToOrigin
+//       />
+//     </Wrapper>
+//   );
+// }
