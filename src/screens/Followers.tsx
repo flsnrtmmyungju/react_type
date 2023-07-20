b03cd5cfacbec836b221a48e9fb0a26ebdfb8419
flsnrtmmyungju <mjmj01617@naver.com>
2023-07-20 15:04:56 +0900

    react-router-dom v6이후 새로고침해도 페이지 바뀌지않게 자식페이지로이동, useOutletContext로 자식페이지에 데이터보내기

--- src/screens/Followers.tsx
+++ src/screens/Followers.tsx
@@ -1,4 +1,12 @@
+import { useOutletContext } from "react-router-dom";
+
+interface IFollowers {
+  nameOfUser: string;
+}
+
 function Followers() {
-  return <h1>Followers</h1>;
+  //useOutletContext:부모에서 데이터 보낸거 받을수있음
+  const { nameOfUser } = useOutletContext<IFollowers>();
+  return <h1>여긴 {nameOfUser}의 Followers</h1>;
 }
 export default Followers;
--- src/screens/users/User.tsx
+++ src/screens/users/User.tsx
@@ -11,10 +11,12 @@ function User() {
         User wiht it {userId} is named: {users[Number(userId) - 1].name}
       </h1>
       <hr />
+      {/* 아래처럼 Link,Outlet로 개발하면 새로고침해도 이페이지그대로 */}
       {/* 상대경로라서 현재경로끝에 추가 */}
       <Link to="followers">See followers</Link>
       {/* 얘만있으면 경로가 바뀌지않으니까 나타나지않음*/}
-      <Outlet />
+      {/* context !!모든!!!자식에게 데이터보낼수있음 */}
+      <Outlet context={{ nameOfUser: users[Number(userId) - 1].name }} />
     </>
   );
 }
