7d0a2f1809d2496230da0115338c3e7d5d8806fc
flsnrtmmyungju <mjmj01617@naver.com>
2023-07-20 14:50:44 +0900

    react-router-dom v6 Outlet사용해서 자식화면에 상대경로로 표시

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,16 +2,21 @@ import { createBrowserRouter } from "react-router-dom";
 import ErrorComponent from "./components/ErrorComponent";
 import Root from "./Root";
 import About from "./screens/About";
+import Followers from "./screens/Followers";
 import Home from "./screens/Home";
 import NotFound from "./screens/NotFound";
 import User from "./screens/users/User";
 
 const router = createBrowserRouter([
+  //"/home" 절대경로 ! 루트아래의 home
+  //"home" 상대걍로 ! 현재 url의 끝에  /home 이 붙음
+
   // 전체라우터의 컨테이너
   {
     path: "/",
     // ex순서 /about접속시//
-    //1.라우터 2.<Root /> 3. < Root /> 안의 < Outlet /> 4. Outlet을 < About />로 대체함
+    //1.라우터 2.<Root /> 3. < Root /> 안의 < Outlet />(자식이있을경우 )
+    //4. Outlet(자식)을 < About /> 로 대체함
     element: <Root />,
     children: [
       {
@@ -29,6 +34,12 @@ const router = createBrowserRouter([
       {
         path: "users/:userId",
         element: <User />,
+        children: [
+          {
+            path: "followers",
+            element: <Followers />,
+          },
+        ],
       },
     ],
     errorElement: <NotFound />,
--- src/screens/users/User.tsx
+++ src/screens/users/User.tsx
@@ -1,13 +1,20 @@
-import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
 import { users } from "../../db";
 
 function User() {
   const { userId } = useParams();
   return (
     <>
+      {" "}
       <h1>
         User wiht it {userId} is named: {users[Number(userId) - 1].name}
       </h1>
+      <hr />
+      {/* 상대경로라서 현재경로끝에 추가 */}
+      <Link to="followers">See followers</Link>
+      {/* 얘만있으면 경로가 바뀌지않으니까 나타나지않음*/}
+      <Outlet />
     </>
   );
 }
